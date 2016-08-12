0d07c6b569710ab2b7a903bb694c7b4639bb4f7e
xc145214 <xc145214@gmail.com>
2016-08-12 15:28:59 +0800

    spring boot 1.4.0存在中文乱码问题待解决

---
+++ properties-config/src/main/java/com/xc/service/BlogProperties.java
@@ -0,0 +1,110 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.xc.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ *
+ *  @author  xiachuan at 2016/8/11 17:12。
+ */
+@Component
+public class BlogProperties {
+
+    @Value("${com.didispace.blog.name}")
+    private String name;
+    @Value("${com.didispace.blog.title}")
+    private String title;
+    @Value("${com.didispace.blog.desc}")
+    private String desc;
+
+    @Value("${com.didispace.blog.value}")
+    private String value;
+    @Value("${com.didispace.blog.number}")
+    private Integer number;
+    @Value("${com.didispace.blog.bignumber}")
+    private Long bignumber;
+    @Value("${com.didispace.blog.test1}")
+    private Integer test1;
+    @Value("${com.didispace.blog.test2}")
+    private Integer test2;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Long getBignumber() {
+        return bignumber;
+    }
+
+    public void setBignumber(Long bignumber) {
+        this.bignumber = bignumber;
+    }
+
+    public Integer getTest1() {
+        return test1;
+    }
+
+    public void setTest1(Integer test1) {
+        this.test1 = test1;
+    }
+
+    public Integer getTest2() {
+        return test2;
+    }
+
+    public void setTest2(Integer test2) {
+        this.test2 = test2;
+    }
+}
+
--- properties-config/src/test/java/com/xc/ApplicationTests.java
+++ properties-config/src/test/java/com/xc/ApplicationTests.java
@@ -14,34 +14,46 @@
  */
 package com.xc;
 
-import com.xc.service.Blog;
+import com.xc.service.BlogProperties;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- *  测试主文件。
+ * 测试主文件。
  *
- *  @author xiachuan at 2016/8/8 16:39。
+ * @author xiachuan at 2016/8/8 16:39。
  */
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApplicationTests {
 
     private static final Log log = LogFactory.getLog(ApplicationTests.class);
 
-
     @Autowired
-    Blog blog;
+    private BlogProperties blogProperties;
 
     @Test
     public void testString() throws Exception {
-        Assert.assertEquals(blog.getName(), "pandaX");
-        Assert.assertEquals(blog.getTitle(), "Spring Boot教程");
+
+
+
+        log.info("随机数测试输出：");
+        log.info("随机字符串 : " + blogProperties.getValue());
+        log.info("随机int : " + blogProperties.getNumber());
+        log.info("随机long : " + blogProperties.getBignumber());
+        log.info("随机10以下 : " + blogProperties.getTest1());
+        log.info("随机10-20 : " + blogProperties.getTest2());
+
+        Assert.assertEquals("pandaX", blogProperties.getName());
+        Assert.assertEquals("Spring Boot Title", blogProperties.getTitle());
+        Assert.assertEquals("pandaX is writing Spring Boot Title", blogProperties.getDesc());
     }
 }
 
