e7e936258041714528f07c749be18699fe9f8736
xc145214 <xc145214@gmail.com>
2016-08-19 10:37:09 +0800

    add test

---
+++ velocity-web/src/test/java/com/xc/VelocityWebAppTests.java
@@ -0,0 +1,53 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.xc;
+
+import com.xc.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ *  test
+ *
+ *  @author xiachuan at 2016/8/19 10:35。
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class VelocityWebAppTests {
+
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+
+    }
+}
+
