76a47934629cc132ce76c56c7e8479f497ab134a
xc145214 <xc145214@gmail.com>
2016-08-19 10:11:15 +0800

    freeMarker app

---
+++ freeMarker-web/src/main/java/com/xc/FreeMarkerWebApp.java
@@ -0,0 +1,15 @@
+package com.xc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class FreeMarkerWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMarkerWebApp.class);
+    }
+}
---
+++ freeMarker-web/src/main/java/com/xc/web/HelloController.java
@@ -0,0 +1,42 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.xc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *  控制器。
+ *
+ *  @author xiachuan at 2016/8/19 9:54。
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "http://blog.didispace.com");
+        return "index";
+    }
+}
+
