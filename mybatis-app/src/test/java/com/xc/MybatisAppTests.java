06049b8d3791aebd130a13b335f9b08713504f48
xc145214 <xc145214@gmail.com>
2016-08-29 18:28:17 +0800

    mybatis 带回滚测试

--- mybatis-app/src/test/java/com/xc/MybatisAppTests.java
+++ mybatis-app/src/test/java/com/xc/MybatisAppTests.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 整合mybatis 测试。
@@ -32,12 +33,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class MybatisAppTests {
 
     @Autowired
     private UserMapper userMapper;
 
 
+    /**
+     * 带回滚测试。
+     * @throws Exception
+     */
     @Test
     @Rollback
     public void findByName() throws Exception {
